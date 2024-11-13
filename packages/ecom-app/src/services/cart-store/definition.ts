54ab51161015d4d3d88d5bd3a677f069b0b74251
Netanel Gilad <netanelg@wix.com>
2024-11-13 14:17:25 +0200

    more nice stuff

--- packages/ecom-app/src/services/cart-store/definition.ts
+++ packages/ecom-app/src/services/cart-store/definition.ts
@@ -1,6 +1,5 @@
-import { BuilderHost } from "@wix/builder-host";
 import { currentCart } from "@wix/ecom";
-import { HostModule } from "@wix/sdk";
+import { defineService } from "@wix/sdk-runtime-extended";
 import { atom } from "nanostores";
 import serviceJson from "./service.json";
 
@@ -9,10 +8,4 @@ export type CartStore = {
   $currentCart: ReturnType<typeof atom<currentCart.Cart | null>>;
 };
 
-export const cartStore: HostModule<CartStore, BuilderHost> & { id: string } = {
-  __type: "host",
-  create(host) {
-    return host.getService(serviceJson.id) as CartStore;
-  },
-  id: serviceJson.id,
-};
+export const cartStore = defineService<CartStore>(serviceJson.id);
--- packages/sdk-runtime-extended/src/index.ts
+++ packages/sdk-runtime-extended/src/index.ts
@@ -1,5 +1,9 @@
-import { BuilderHost } from "@wix/builder-host";
-import { WixClient } from "@wix/sdk";
+import {
+  AuthenticationStrategy,
+  createClient,
+  Host,
+  WixClient,
+} from "@wix/sdk";
 import { HostModule } from "@wix/sdk-runtime/host-modules";
 
 export function provide<T>(
@@ -7,4 +11,52 @@ export function provide<T>(
   factory: (client: WixClient) => T
 ) {
   return factory;
+}
+
+export function defineService<T>(
+  id: string
+): HostModule<T, BuilderHost> & { id: string } {
+  return {
+    __type: "host",
+    create(host) {
+      return host.getService(id) as T;
+    },
+    id,
+  };
+}
+
+export type BuilderHost = Host & {
+  getService<T>(extensionId: string): T;
+};
+
+export function createBuilderHost(
+  servicesMap: Map<
+    string, // extension id
+    (client: WixClient) => unknown
+  >,
+  auth: AuthenticationStrategy
+): BuilderHost {
+  const initializedServices = new Map<
+    string, // extension id
+    unknown
+  >();
+  return {
+    getService<T>(extensionId: string): T {
+      if (!initializedServices.has(extensionId)) {
+        initializedServices.set(
+          extensionId,
+          servicesMap.get(extensionId)!(createClient({ auth, host: this }))
+        );
+      }
+      return initializedServices.get(extensionId) as T;
+    },
+    channel: {
+      observeState(callback) {
+        callback({}, {});
+        return {
+          disconnect() {},
+        };
+      },
+    },
+  };
 }
